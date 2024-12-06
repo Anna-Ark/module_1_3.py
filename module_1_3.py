7145befea1ed714e21d98ada3baa6568eeadd233
Anna-Ark <Anna.eavd@gmail.com>
2024-12-06 05:29:28 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+name ="Anna"
+print(name, type(name))
+name=5
+print(name, type(name))
+name= 5.5
+print(name, type(name))
+name= [1 , 2]
+print(name, type(name))
+age = 39
+new_age = 1
+print(age +   new_age)
+name='Is Student'
+print('Is Student')
